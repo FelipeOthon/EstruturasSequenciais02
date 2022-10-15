27972c34375fd164b7ddf004790eb1597093ad7a
FelipeOthon <felipeosantiago@gmail.com>
2022-10-14 22:37:57 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Digite um numero:");
+            int numero = sc.nextInt();
+            sc.close();
+
+            System.out.println("o numero digitado foi: "+ numero);
+    }
+}
